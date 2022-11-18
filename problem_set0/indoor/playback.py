651ea1bad2ab547f758878fe46990c343407efad
AlexRomashow <alexromashow7@gmail.com>
2022-11-18 15:30:00 +0200

    playback.py

---
+++ problem_set0/indoor/playback.py
@@ -0,0 +1,12 @@
+"""
+In a file called playback.py, implement a program in 
+Python that prompts the user for input and then outputs 
+that same input, replacing each space with ... 
+(i.e., three periods).
+"""
+
+# User input and replacing each space with ...
+# using replace function
+user_input = input().replace(' ', '...')
+# Output result
+print(user_input)
